ef1fa945e9c283b866d1087be4c59b6513e587ea
eun-younglee <59904000+eun-younglee@users.noreply.github.com>
2022-04-04 11:57:10 +0900

    Change names
    So to keep same as the book

--- this_is_ct/ch6-3.py
+++ this_is_ct/ch6-3.py
@@ -1,32 +1,14 @@
-# 퀵 정렬
-# 가장 많이 사용됨
-# 기준 데이터를 설정하고 그 기준보다 큰 데이터와 작은 데이터의 위치를 바꾸기
-# divide & conquer 알고리즘
-# 데이터가 무작위적일 때 더 잘 동작한다
+# 삽입 정렬
+# 데이터를 하나씩 확인하여 각 데이터를 적절한 위치에 삽입하기
+# 필요할 때만 위치를 바꾸기 때문에 데이터가 거의 정렬되어 있을 때 효율적
+# 두 번째 데이터부터 시작(첫번째 데이터는 정렬되어 있다고 판단)
 
 nums = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-
-def quick_sort(nums, start, end):
-    if start >= end:  # if there is only one element, end the sorting
-        return
-    pivot = start  # pivot is the first element
-    left = start + 1
-    right = end
-    while left <= right:  # until left is smaller than the right
-        while left <= end and nums[left] <= nums[pivot]:  # until finding bigger number than pivot
-            left += 1
-        while right > start and nums[right] >= nums[pivot]:  # until finding smaller number than pivot
-            right -= 1
-        if left > right:  # if left and right is crossed
-            # switch because there are smaller elements than pivot
-            nums[right], nums[pivot] = nums[pivot], nums[right]
-        else:  # not crossed
-            # pivot is the smallest number
-            nums[left], nums[right] = nums[right], nums[left]
-    quick_sort(nums, start, right - 1)  # quick sort fore elements
-    quick_sort(nums, right + 1, end)  # quick sort rear elements
-
-
-quick_sort(nums, 0, len(nums) - 1)
+for i in range(1, len(nums)):
+    for j in range(i, 0, -1):  # move to the left
+        if nums[j - 1] > nums[j]:  # if current is smaller than previous one
+            nums[j], nums[j - 1] = nums[j - 1], nums[j]  # swap
+        else:  # stop if you meet smaller data than current
+            break
 print(nums)
---
+++ this_is_ct/ch6-4.py
@@ -0,0 +1,33 @@
+# 퀵 정렬
+# 가장 많이 사용됨
+# 기준 데이터를 설정하고 그 기준보다 큰 데이터와 작은 데이터의 위치를 바꾸기
+# divide & conquer 알고리즘
+# 데이터가 무작위적일 때 더 잘 동작한다
+
+nums = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+
+def quick_sort(nums, start, end):
+
+    if start >= end:  # if there is only one element, end the sorting
+        return
+    pivot = start  # pivot is the first element
+    left = start + 1
+    right = end
+    while left <= right:  # until left is smaller than the right
+        while left <= end and nums[left] <= nums[pivot]:  # until finding bigger number than pivot
+            left += 1
+        while right > start and nums[right] >= nums[pivot]:  # until finding smaller number than pivot
+            right -= 1
+        if left > right:  # if left and right is crossed
+            # switch smallest data and pivot, so numbers before pivot is all smaller than pivot
+            nums[right], nums[pivot] = nums[pivot], nums[right]
+        else:  # not crossed
+            # switch left and right because left is smaller than pivot and right is bigger than pivot
+            nums[left], nums[right] = nums[right], nums[left]
+    quick_sort(nums, start, right - 1)  # quick sort fore elements
+    quick_sort(nums, right + 1, end)  # quick sort rear elements
+
+
+quick_sort(nums, 0, len(nums) - 1)
+print(nums)
