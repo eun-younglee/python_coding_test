07b3b6a747547f716605a3b1ead8b94ed80ac298
eun-younglee <59904000+eun-younglee@users.noreply.github.com>
2022-06-03 16:55:51 +0900

    Update 14. 외벽 점검.py

--- this_is_ct/Part 3. Past Questions/14. 외벽 점검.py
+++ this_is_ct/Part 3. Past Questions/14. 외벽 점검.py
@@ -2,19 +2,36 @@
 # 사람 별 1시간 동안 이동할 수 있는 거리가 제공되었을 때
 # 시계 혹은 반시계 방향으로 외벽을 따라 움직여
 # 취약 지점을 점검하기 위해 보내야 하는 친구 수의 최솟값
+from itertools import permutations
+
+def solution(n, weak, dist):
+    length = len(weak)
+    for i in range(length):
+        weak.append(weak[i] + n)  # expend the weak
+    answer = len(dist) + 1  # initialisation
+    for start in range(length):
+        # check every permutation
+        for friends in list(permutations(dist, len(dist))):
+            count = 1
+            # last position for the friend to check
+            position = weak[start] + friends[count - 1]
+            # check all weak points
+            for index in range(start, start + length):
+                # out of distance
+                if position < weak[index]:
+                    count += 1  # add a friend
+                    if count > len(dist):  # end if friend count exceed number of friends
+                        break
+                    position = weak[index] + friends[count - 1]
+            answer = min(answer, count)
+        if answer > len(dist):
+            return -1
+    return answer
 
 n = int(input())
 # weak = list(map(int, input().split()))
 # dist = list(map(int, input().split()))
-# weak = [1, 5, 6, 10]
-# dist = [1, 2, 3, 4]
 weak = [1, 3, 4, 9, 10]
 dist = [3, 5, 7]
-dist_dist = []
-for i in range(len(weak) - 1, -1, - 1):
-    dist_dist.append((weak[i] - weak[i - 1] + n) % n)
-print(dist_dist)
-
-if max(dist) >= sum(dist_dist) - max(dist_dist):  # can be done by one person
-    print(1)
-
+answer = solution(n, weak, dist)
+print(answer)
